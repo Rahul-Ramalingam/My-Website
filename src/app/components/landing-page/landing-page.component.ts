d88a318acf4ccb0f8c2543822329071977cc73ee
Rahul Ram <rahulram30122000@gmail.com>
2022-08-16 00:21:58 +0530

    landing page completed

---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+  roles:string[] = ["Full Stack Developer", "Flutter App Dev", "AI Enthusiast", "UI/UX Designer"]
+  rolesPointer:number = 0
+  constructor() { }
+
+  ngOnInit(): void {
+    const colors = ["#00FFF0", "#D9D9D9", "#F45656", "#FF9900", "#FFC700"];
+    generateRandomParticles(25,colors);
+    setTimeout(() =>{     
+      this.type(document.getElementById('replace'), this.roles[this.rolesPointer],0);
+   },1000);   
+  }
+
+  openContactForm(){
+    console.log("contact")
+  }
+
+  type($el:any, text:string, position:number) {
+    if (text.length >= position) {
+      var rchars = 'qwerty';
+      if (position % 3 == 0 && Math.random() > 0.9) {
+        var typo;
+        var chr = text.substr(position, 1);
+        if (chr == chr.toUpperCase()) { typo = chr.toLowerCase(); }
+        else { typo = rchars.substr(Math.floor(Math.random() * rchars.length), 1); }
+        $el.innerHTML = text.substring(0, position - 1) + typo + '_';
+        setTimeout(() => { this.type($el, text, position - 1); }, 200)
+      }
+      else {
+        $el.innerHTML = (text.substring(0, position) + '_');
+        setTimeout(() => { this.type($el, text, position + 1); }, 150)
+      }
+    }
+    else {
+      setTimeout(() => { this.erase($el, text, text.length); }, 400)
+    }
+  }
+  
+  erase(this: any, $el:any, text:string, position:number){
+    if (position >= 0) {
+      $el.innerHTML = (text.substring(0,position) + '_');
+      setTimeout(() => { this.erase($el, text, position - 1); }, 150)
+    } else {
+      if(this.rolesPointer < this.roles.length - 1){
+        this.rolesPointer += 1;
+        setTimeout(() => { this.type($el, this.roles[this.rolesPointer], 0); }, 400)
+      }else{
+        this.rolesPointer = 0;
+        setTimeout(() => { this.type($el, this.roles[this.rolesPointer], 0); }, 400)
+      }      
+    }
+  }
+}
+
+function generateRandomParticles(numBalls: number, colors:string[]) {
+  const balls = [];
+
+    //create partices
+    for (let i = 0; i < numBalls; i++) {
+      let ball = document.createElement("div");
+      ball.classList.add("ball");
+      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+      ball.style.transform = `scale(${Math.random()*1.2})`;
+      ball.style.width = `${Math.random()*2}em`;
+      ball.style.height = ball.style.width;
+      ball.style.position = `absolute`;
+      ball.style.borderRadius = `100%`;
+      ball.style.opacity = `0.7`;
+      balls.push(ball);
+      document.getElementById("landing_page")?.append(ball);
+    }
+
+    // animation
+    balls.forEach((el, i, ra) => {
+      let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
+      };
+
+      let anim = el.animate(
+        [
+          { transform: "translate(0, 0)" },
+          { transform: `translate(${to.x}vw, ${to.y}vh)` }
+        ],
+        {
+          duration: (Math.random() + 1) * 20000,
+          direction: "alternate",
+          fill: "both",
+          iterations: Infinity,
+          easing: "ease-in-out"
+        }
+      );
+    });
+}
+
+
+
+
