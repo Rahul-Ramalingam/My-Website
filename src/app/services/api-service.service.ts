921c0146fa4ecff3c1036bcff61be4e2b2e3151a
Rahul Ram <rahulram30122000@gmail.com>
2022-08-29 00:50:07 +0530

    general styling

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -18,7 +18,7 @@ export class ApiServiceService {
     blog.hashtags = ["ComputerVision", "Python", "OpenCV"];
     blog.title = "Introduction to OpenCV";
     blog.updatedTime = "28/8/2022";
-    blog.summary = "Thats how Im feeling about the Biden Administration at the moment. This past winter, Biden seemed cursed."
+    blog.summary = "Thats how Im feeling about the Biden Administration at the moment. This past winter, Biden seemed cursed. Despite delivering a trillion dollars in direct aid to millions with the American Rescue Plan and then a similar amount in investment in domestic needs with the Bipartisan Infrastructure Act, his presidency seemed completely stuck in a ditch."
     
     return [blog,blog,blog];
   }
